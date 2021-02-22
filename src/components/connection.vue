0981be25f8bdeade4eac3eb954c62995d387fdbf
azi <1823264684@qq.com>
2021-02-22 11:42:35 +0800

    fix some bug

--- src/components/connection.vue
+++ src/components/connection.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="connection">
     <div class="tips">
-      欢迎您！为提高服务效率，请阁下在开始对话前提供一些基本资料
+      {{ title }}
     </div>
     <el-form
       :model="ruleForm"
@@ -11,43 +11,49 @@
       class="demo-ruleForm"
     >
       <el-form-item prop="name">
-        <el-input v-model="ruleForm.name" placeholder="* 姓名"></el-input>
+        <el-input :maxlength="10"  v-model="ruleForm.name" placeholder="* 姓名"></el-input>
       </el-form-item>
       <el-form-item prop="phone">
         <el-input :maxlength="11" v-model="ruleForm.phone" placeholder="* 手机号"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-input v-model="ruleForm.email" placeholder="邮箱"></el-input>
+        <el-input v-model="ruleForm.email" placeholder="邮箱地址"></el-input>
+      </el-form-item>
+      <el-form-item prop="text" v-if="props.isLeaveMassage">
+        <el-input placeholder="*留言内容" type="textarea" v-model="ruleForm.text"></el-input>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="submitForm('ruleForm')">
-          开始会话
+          {{state.btnTitle}}
         </el-button>
       </el-form-item>
     </el-form>
   </div>
 </template>
 <script setup>
-import { reactive, ref, onMounted } from "vue";
+import { reactive, ref, onMounted, defineProps, watch, computed } from "vue";
 import { useRouter } from "vue-router";
-
+// 路由
 const router = useRouter();
 
-const form = ref(null)
+const props = defineProps({
+  isLeaveMassage: false
+})
 
-const  checkPhone = (rule, value, callback) => {
+const form = ref(null)
+const checkPhone = (rule, value, callback) => {
   if (!/^1[3|4|5|7|8|9][0-9]{9}$/.test(value) || !value) {
     callback(new Error('请输入11位正确的手机号'))
   } else {
     callback()
   }
 }
-
 const state = reactive({
   ruleForm: {
     phone: '',
     name: '',
     email: '',
+    text: '',
   },
   rules: {
     name: [
@@ -56,10 +62,13 @@ const state = reactive({
     phone: [
       { required: true, validator: checkPhone, trigger: 'blur' },
     ],
-  }
+    text: [
+      { required: true, message: '请输入留言内容', trigger: 'blur' }
+    ]
+  },
+  btnTitle: ''
 });
 const ruleForm = state.ruleForm, rules = state.rules;
-
 
 function submitForm() {
   form.value.validate((valid) => {
@@ -74,22 +83,39 @@ function submitForm() {
   })
 }
 
+// // 监听isConnect
+// watch(isConnect, (val, oldVal) => {
+//   console.log(val, 'isConnect');
+// })
+
+const title = computed(() => {
+  let value = '欢迎您！为提高服务效率，请阁下在开始对话前提供一些基本资料。';
+  state.btnTitle = '开始会话';
+  if (props.isLeaveMassage) {
+   value = '抱歉，我们的客服人员目前离线。请留言，我们将会与您联系。';
+    state.btnTitle = '留言';
+  }
+  return value
+})
+
 onMounted(() => {
   form.value && form.value.resetFields()
 })
 </script>
 <style lang='scss'>
 .connection {
- 
- .tips {
-   font-size: 14px;
-   text-align: left;
-   color: #666666;
-   padding-bottom: 30px;
- }
+  
+  .tips {
+    font-size: 14px;
+    text-align: left;
+    color: #666666;
+    padding-bottom: 30px;
+  }
+  
   .el-form-item {
     margin-bottom: 40px;
   }
+  
   .el-button {
     width: 100%;
     margin-top: 40px;
--- src/views/home.vue
+++ src/views/home.vue
@@ -9,56 +9,176 @@
     <div class="header">
       {{ state.header }}
     </div>
-    <component class="component"
-               :is="comps.compsName"></component>
+    <connection class="component" :isLeaveMassage="state.isLeaveMassage"></connection>
+<!--    <chat class="component"></chat>-->
+<!--    TODO: 暂时不使用动态组件，报警告，要求要用非响应式，有强迫症暂时不用-->
+<!--    <component class="component"-->
+<!--               :isLeaveMassage="state.isLeaveMassage"-->
+<!--               :is="comps.compsName"></component>-->
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted, toRefs, watch, reactive } from "vue";
 import { useRoute } from 'vue-router';
+import { dateFormat } from 'common/util';
 // 会话前，预留信息
 import connection from 'components/connection.vue';
-import { dateFormat } from 'common/util';
 // 聊天页面
-// import chat from 'components/chat.vue';
-// 客服不在线，留言
-import leaveMassage from 'components/leaveMassage.vue';
+import chat from 'components/chat.vue';
+import WebsocketHeartbeatJs from "websocket-heartbeat-js";
 
 const router = useRoute();
 
-const comps = ref({
-  compsName: connection
-})
+// const comps = reactive({
+//   compsName: connection, // 动态组件
+// })
+
+// 基本信息
 const state = reactive({
-  header: '基本资料',
+  header: '正在连接客服...',
+  isLeaveMassage: false, // 是否处于留言状态
   // 客户信息
   userInfo: {
     phone: '',
     name: '',
     email: '',
   },
-  isShowChat: false, // 在线聊天或留言
+});
+
+// 聊天
+const chatInfo = reactive({
+  charSocket: null,
+  url: 'wss://wst.fivehills.com/wss/', // socket url
+  apiKey: 'alpha',
+  serviceInfo: {}, // 客服信息
+  charLog: [],  // 聊天记录
+  random: '', // 10位随机值
+  isConnect: false, // 连接客服是否成功
+  isServiceMsg: false,  // 已显示客服回复的默认信息
 })
+
 // 根据路由变化，出发赋值
 watch(router, () => {
   changeComps();
+  if(state.userInfo.name&&state.userInfo.phone) {
+    websocketHandle();
+  }
 })
-
+// 处理信息和留言
 function changeComps() {
   const { name, phone, email } = router.query
   state.userInfo.name = name || '';
   state.userInfo.phone = phone || '';
   state.userInfo.email = email || '';
-  if (!state.userInfo.name) {
-    comps.value.compsName = connection // 会话前，预留信息
+  if (!chatInfo.isConnect && name && phone) {
+    state.isLeaveMassage = true;
+    state.header = '给我的留言';
   } else {
-    comps.value.compsName = leaveMassage // 客服不在线，留言
+    state.isLeaveMassage = false;
+    state.header = '正在连接客服...';
+  }
+}
+function getId() {
+  return Date.now() + Math.random().toString().substr(2, 10)
+}
+//
+function websocketHandle() {
+  const params = chatInfo.apiKey + `&${state.userInfo.name}&` + chatInfo.random;
+  console.log(chatInfo.url + params, '888', state.userInfo.name);
+  chatInfo.charSocket = new WebsocketHeartbeatJs({
+    url: chatInfo.url + params,
+    pingTimeout: 20000,
+    pongTimeout: 20000,
+    pingMsg: `{"from": "${params}", "sys": "CPT", "to":"client"}`
+  })
+  chatInfo.charSocket.onreconnect = () => {
+    console.log('charSocket reconnecting...')
+  }
+  chatInfo.charSocket.onerror = () => {
+    console.log('charSocket onerror...')
+  }
+  chatInfo.charSocket.onclose = () => {
+    console.log('charSocket onerror...')
+  }
+  chatInfo.charSocket.onmessage = ({data}) => {
+    let obj = {
+      data: {
+        online_service: []
+      }
+    }
+    try {
+      obj = JSON.parse(data)
+    } catch (e) {
+      console.log('格式化错误')
+    }
+    if (obj.code === undefined) return
+    // 首次连接
+    if (obj.code === 0) {
+      if (obj.data.online_service && obj.data.online_service.length > 0) {
+        chatInfo.isConnect = true
+        chatInfo.serviceInfo = obj.data.online_service[0]
+        if (chatInfo.isServiceMsg) return
+        chatInfo.isServiceMsg = true
+        chatInfo.charLog.push({
+          id: getId(),
+          time: dateFormat("YYYY-mm-dd HH:MM:SS", new Date()),
+          text: obj.data.greeting,
+          isUser: false,
+          status: 0
+        })
+        
+      } else {
+        chatInfo.isShowChar = false
+        chatInfo.serviceInfo = {}
+      }
+      return
+    }
+    // 发送成功
+    if (obj.code === 2 && obj.data.data === 'replyCodeConfirm') {
+      // 将对应的loading状态修改
+      const index = chatInfo.charLog.findIndex(item => item.id === obj.data.msgId)
+      if (index < 0) return
+      setTimeout(() => {
+        chatInfo.charLog[index].status = 0
+      }, 100)
+      return
+    }
+    // 客服回复
+    if (obj.code === 2) {
+      const time = new Date(obj.data.time).getTime() + 8 * 60 * 60 * 1000
+      const isImg = /^data:image/.test(obj.data.data)
+      chatInfo.charLog.push({
+        type: isImg ? 'image' : 'text',
+        id: chatInfo.getId(),
+        time: dateFormat("YYYY-mm-dd HH:MM:SS", new Date(time)),
+        text: isImg ? obj.data.image : obj.data.data,
+        isUser: false,
+        status: 0
+      })
+      // chatInfo.$nextTick(_ => {
+      //   chatInfo.getDom('charLog').scrollTo(0, this.getDom('charLog').scrollHeight)
+      // })
+      return
+    }
+    // 心跳更新客服列表
+    if (obj.code === 3) {
+      if (obj.data.online_service && obj.data.online_service.length > 0) {
+        chatInfo.isShowChar = true
+        chatInfo.serviceInfo = obj.data.online_service[0]
+      } else {
+        chatInfo.isShowChar = false
+        chatInfo.serviceInfo = {}
+      }
+    }
   }
 }
 
+
 onMounted(() => {
-  changeComps()
+  chatInfo.random = localStorage.getItem('random') ? localStorage.getItem('random') : Math.random().toString().substr(2, 10)
+  localStorage.setItem('random', chatInfo.random)
+  changeComps();
 })
 </script>
 
